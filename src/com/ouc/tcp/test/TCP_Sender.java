01ea43566ff49443aa0d7c250487adaad15e21ae
VastCosmic <137017677@qq.com>
2024-01-08 17:03:39 +0800

    RDT 3.0

--- src/com/ouc/tcp/test/TCP_Sender.java
+++ src/com/ouc/tcp/test/TCP_Sender.java
@@ -6,82 +6,88 @@ package com.ouc.tcp.test;
 import com.ouc.tcp.client.TCP_Sender_ADT;
 import com.ouc.tcp.client.UDT_RetransTask;
 import com.ouc.tcp.client.UDT_Timer;
-import com.ouc.tcp.message.*;
-import com.ouc.tcp.tool.TCP_TOOL;
+import com.ouc.tcp.message.TCP_PACKET;
+
 
 public class TCP_Sender extends TCP_Sender_ADT {
-	
-	private TCP_PACKET tcpPack;	//待发送的TCP数据报
+
+	private TCP_PACKET tcpPack;    //待发送的TCP数据报
 	private volatile int flag = 0;
-	
+
+	// 定时器
+	UDT_Timer timer;
+	// 重传任务
+	UDT_RetransTask task;
+
 	/*构造函数*/
 	public TCP_Sender() {
-		super();	//调用超类构造函数
-		super.initTCP_Sender(this);		//初始化TCP发送端
+		super();    //调用超类构造函数
+		super.initTCP_Sender(this);        //初始化TCP发送端
 	}
-	
+
 	@Override
 	//可靠发送（应用层调用）：封装应用层数据，产生TCP数据报；需要修改
 	public void rdt_send(int dataIndex, int[] appData) {
-		
 		//生成TCP数据报（设置序号和数据字段/校验和),注意打包的顺序
 		tcpH.setTh_seq(dataIndex * appData.length + 1);//包序号设置为字节流号：
 		tcpS.setData(appData);
-		tcpPack = new TCP_PACKET(tcpH, tcpS, destinAddr);		
-		//更新带有checksum的TCP 报文头		
+		tcpPack = new TCP_PACKET(tcpH, tcpS, destinAddr);
+		//更新带有checksum的TCP 报文头        
 		tcpH.setTh_sum(CheckSum.computeChkSum(tcpPack));
 		tcpPack.setTcpH(tcpH);
-		
+
 		//发送TCP数据报
 		udt_send(tcpPack);
 		flag = 0;
-		
+
+		// 设置重传任务, 每隔1s重传一次
+		timer = new UDT_Timer();
+		task = new UDT_RetransTask(client, tcpPack);
+		timer.schedule(task, 1000, 1000);
+
 		//等待ACK报文
 		//waitACK();
-		while (flag==0);
+		while (flag == 0) ;
 	}
-	
+
 	@Override
 	//不可靠发送：将打包好的TCP数据报通过不可靠传输信道发送；仅需修改错误标志
 	public void udt_send(TCP_PACKET stcpPack) {
 		//设置错误控制标志
-		tcpH.setTh_eflag((byte)1);
-		System.out.println("to send: "+stcpPack.getTcpH().getTh_seq());
+		tcpH.setTh_eflag((byte) 4);
+		System.out.println("to send: " + stcpPack.getTcpH().getTh_seq());
 		//发送数据报
 		client.send(stcpPack);
 	}
-	
+
+
 	@Override
-	//需要修改
 	public void waitACK() {
 		//循环检查ackQueue
-		//循环检查确认号对列中是否有新收到的ACK		
+		//循环检查确认号对列中是否有新收到的ACK
 		if(!ackQueue.isEmpty()){
 			int currentAck=ackQueue.poll();
 			// System.out.println("CurrentAck: "+currentAck);
 
-			// N ACK
-			if (currentAck != tcpPack.getTcpH().getTh_seq()){
-				// 重新发送，保持waitACK状态
-				System.out.println("NACK: "+tcpPack.getTcpH().getTh_seq());
-				udt_send(tcpPack);
-				flag = 0;
-			} else {
-				// ACK, 切换到rdt_send状态，等待下一个数据包或应用层调用
-				System.out.println("Clear: "+tcpPack.getTcpH().getTh_seq());
-				flag = 1;
-			}
-
-//			// 原始代码：
-//			if (currentAck == tcpPack.getTcpH().getTh_seq()){
-//				System.out.println("Clear: "+tcpPack.getTcpH().getTh_seq());
-//				flag = 1;
-//				//break;
-//			}else{
-//				System.out.println("Retransmit: "+tcpPack.getTcpH().getTh_seq());
+//			// N ACK
+//			if (currentAck != tcpPack.getTcpH().getTh_seq()){
+//				// 重新发送，保持waitACK状态
+//				System.out.println("NACK: "+tcpPack.getTcpH().getTh_seq());
 //				udt_send(tcpPack);
 //				flag = 0;
+//			} else {
+//				// ACK, 切换到rdt_send状态，等待下一个数据包或应用层调用
+//				System.out.println("Clear: "+tcpPack.getTcpH().getTh_seq());
+//				flag = 1;
 //			}
+
+			// RDT 3.0
+			if (currentAck == tcpPack.getTcpH().getTh_seq()){
+				// ACK, 切换到rdt_send状态，等待下一个数据包或应用层调用
+				System.out.println("Clear: "+tcpPack.getTcpH().getTh_seq());
+				timer.cancel();
+				flag = 1;
+			}
 		}
 	}
 
